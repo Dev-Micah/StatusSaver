49bc228caeacdc5e6b0f5236daa7b0018ed37ec2
Micah Nyabuto <atembamicah@gmail.com>
2025-05-17 13:13:00 +0300

    Added HomeScreen

--- app/src/main/java/com/micahnyabuto/statussaver/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/micahnyabuto/statussaver/ui/screens/home/HomeScreen.kt
@@ -11,15 +11,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 
 @Composable
-fun HomeScreen(){
+fun HomeScreen(
+    navController: NavController
+){
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ){
-
-        Spacer(Modifier.height(20.dp))
-        CircularProgressIndicator()
+        Text("HomeScreen")
     }
 }
