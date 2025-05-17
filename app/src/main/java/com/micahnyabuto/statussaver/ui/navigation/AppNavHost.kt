205e5a045a6420caf3d997cbff05bdb4001f4dc7
Micah Nyabuto <atembamicah@gmail.com>
2025-05-17 13:19:46 +0300

    Added BottomNavigation

--- app/src/main/java/com/micahnyabuto/statussaver/ui/navigation/AppNavHost.kt
+++ app/src/main/java/com/micahnyabuto/statussaver/ui/navigation/AppNavHost.kt
@@ -2,31 +2,51 @@ package com.micahnyabuto.statussaver.ui.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.micahnyabuto.statussaver.ui.screens.home.HomeScreen
 import com.micahnyabuto.statussaver.ui.screens.saved.SavedScreen
 import com.micahnyabuto.statussaver.ui.screens.settings.SettingsScreen
+import com.micahnyabuto.statussaver.ui.screens.splash.SplashScreen
 
 @Composable
 fun AppNavHost(
+    navController: NavHostController,
     modifier: Modifier= Modifier
 ){
-    val navController= rememberNavController()
+    //val navController = rememberNavController()
     NavHost(
         modifier = modifier,
         navController = navController,
-        startDestination = Destinations.Home
+        startDestination = Destinations.Splash
     ) {
+        composable <Destinations.Splash>{
+            SplashScreen(
+                onSplashFinished = {
+                    navController.navigate(Destinations.Home){
+                        popUpTo(Destinations.Splash){
+                            inclusive =true
+                        }
+                    }
+                }
+            )
+        }
         composable <Destinations.Home>{
-            HomeScreen()
+            HomeScreen(
+                navController =navController
+            )
         }
         composable <Destinations.Saved>{
-            SavedScreen()
+            SavedScreen(
+                navController =navController
+            )
         }
         composable <Destinations.Settings>{
-            SettingsScreen()
+            SettingsScreen(
+                navController =navController
+            )
         }
     }
 }
