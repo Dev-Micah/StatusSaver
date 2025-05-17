cd64ed87a5f31e64bbdc03753f96922050760f0a
Micah Nyabuto <atembamicah@gmail.com>
2025-05-17 13:12:13 +0300

    Added Colors

--- app/src/main/java/com/micahnyabuto/statussaver/ui/theme/Color.kt
+++ app/src/main/java/com/micahnyabuto/statussaver/ui/theme/Color.kt
@@ -2,10 +2,22 @@ package com.micahnyabuto.statussaver.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val PrimaryColor = Color(0xFF075E54)
+val PrimaryLightColor = PrimaryColor.copy(alpha = 0.75f)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val SecondaryColor = Color(0xFF075E10)
+val SecondaryLightColor = SecondaryColor.copy(alpha = 0.75f)
+
+val PrimaryTextColor = Color(0xffffffff)
+val SecondaryTextColor = Color(0xff000000)
+
+val SurfaceDark = Color(0xff161616)
+
+val SurfaceLight = Color(0xfff5f5f5)
+
+val BackgroundLightColor = Color(0xfff5f5f5)
+
+val BackgroundDarkColor = Color(0xff101010)
+
+val ErrorColor = Color(0xfff44336)
+val OnErrorColor = Color(0xff000000)
