052aeba1c1f8aa918c11554e2366a89e38bf13cf
Micah Nyabuto <atembamicah@gmail.com>
2025-05-17 13:13:31 +0300

    Added SavedScreen

--- app/src/main/java/com/micahnyabuto/statussaver/ui/screens/saved/SavedScreen.kt
+++ app/src/main/java/com/micahnyabuto/statussaver/ui/screens/saved/SavedScreen.kt
@@ -1,13 +1,20 @@
 package com.micahnyabuto.statussaver.ui.screens.saved
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import java.nio.file.WatchEvent
 
 @Composable
-fun SavedScreen(){
+fun SavedScreen(
+    navController: NavController
+){
     Box(
+        modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ){
         Text("Saved Screen")
