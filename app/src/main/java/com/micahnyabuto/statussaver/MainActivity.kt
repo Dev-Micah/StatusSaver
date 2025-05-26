1482a696a449d33a0d42515e64109fcc42df6a34
Micah Nyabuto <atembamicah@gmail.com>
2025-05-26 23:52:56 +0300

    Update

--- app/src/main/java/com/micahnyabuto/statussaver/MainActivity.kt
+++ app/src/main/java/com/micahnyabuto/statussaver/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.micahnyabuto.statussaver
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -15,6 +20,7 @@ import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -24,23 +30,26 @@ import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.surfaceColorAtElevation
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.micahnyabuto.statussaver.ui.navigation.AppNavHost
 import com.micahnyabuto.statussaver.ui.navigation.BottomNavigation
 import com.micahnyabuto.statussaver.ui.navigation.Destinations
-import com.micahnyabuto.statussaver.ui.screens.home.HomeScreen
-import com.micahnyabuto.statussaver.ui.screens.splash.SplashScreen
+import com.micahnyabuto.statussaver.ui.screens.settings.SettingsViewModel
+import com.micahnyabuto.statussaver.ui.screens.viewmodel.StatusViewModel
 import com.micahnyabuto.statussaver.ui.theme.StatusSaverTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -50,7 +59,40 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            StatusSaverTheme {
+            val viewModel: StatusViewModel = hiltViewModel()
+
+            val context = LocalContext.current
+            val activity = context as Activity
+
+            val launcher = rememberLauncherForActivityResult(
+                contract = ActivityResultContracts.OpenDocumentTree()
+            ) { uri: Uri? ->
+                uri?.let {
+                    // Persist access permissions
+                    context.contentResolver.takePersistableUriPermission(
+                        it,
+                        Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+                    )
+                    // Save URI to shared prefs or notify ViewModel
+                    viewModel.saveSafUri(it)
+                }
+            }
+
+             // Launch the picker somewhere in UI:
+          Button(onClick = { launcher.launch(null) }) {
+                Text("Pick WhatsApp Status Folder")
+           }
+
+            LaunchedEffect(viewModel.safUri.collectAsState().value) {
+                if (viewModel.safUri.value != null) {
+                    viewModel.loadStatusesFromStorage()
+                }
+            }
+            val settingsViewModel : SettingsViewModel =hiltViewModel()
+            val isDarkMode by settingsViewModel.isDarkMode.collectAsState()
+            StatusSaverTheme (
+                darkTheme = isDarkMode
+            ){
                 val navController =rememberNavController()
                 val navBackStackEntry by navController.currentBackStackEntryAsState()
                 val currentRoute = navBackStackEntry?.destination?.route ?: Destinations.Home::class.qualifiedName.orEmpty()
