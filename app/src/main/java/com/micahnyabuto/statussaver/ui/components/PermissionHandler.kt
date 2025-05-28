5a410daf981b73cd3d8787acbf3bb4073182d3c1
Micah Nyabuto <atembamicah@gmail.com>
2025-05-28 13:47:40 +0300

    Added SAF handling permissions

--- app/src/main/java/com/micahnyabuto/statussaver/ui/components/PermissionHandler.kt
+++ app/src/main/java/com/micahnyabuto/statussaver/ui/components/PermissionHandler.kt
@@ -1,56 +1,46 @@
 package com.micahnyabuto.statussaver.ui.components
 
+import android.Manifest
 import android.os.Build
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
-
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun RequestStoragePermissions(onPermissionsGranted: () -> Unit) {
-    val context = LocalContext.current
-
-    val permissions = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-        listOf(
-            android.Manifest.permission.READ_MEDIA_IMAGES,
-            android.Manifest.permission.READ_MEDIA_VIDEO,
-        )
-    } else {
-        listOf(
-            android.Manifest.permission.READ_EXTERNAL_STORAGE,
-            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
-        )
+    val permissions = when {
+        Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> {
+            listOf(
+                Manifest.permission.READ_MEDIA_IMAGES,
+                Manifest.permission.READ_MEDIA_VIDEO
+            )
+        }
+        Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q -> {
+            listOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+        }
+        else -> {
+            listOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+        }
     }
 
-    val multiplePermissionsState = rememberMultiplePermissionsState(permissions)
+    val permissionsState = rememberMultiplePermissionsState(permissions)
 
     LaunchedEffect(Unit) {
-        multiplePermissionsState.launchMultiplePermissionRequest()
+        if (!permissionsState.allPermissionsGranted) {
+            permissionsState.launchMultiplePermissionRequest()
+        }
     }
 
-    if (multiplePermissionsState.allPermissionsGranted) {
-        onPermissionsGranted()
-    } else {
-        Box (
-            Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ){
-            Button(
-                onClick = {multiplePermissionsState.launchMultiplePermissionRequest()}
-            ) {
-            Text("Grant permissions to continue.")
-            }
+    LaunchedEffect(permissionsState.allPermissionsGranted) {
+        if (permissionsState.allPermissionsGranted) {
+            onPermissionsGranted()
         }
-
     }
 }
-
