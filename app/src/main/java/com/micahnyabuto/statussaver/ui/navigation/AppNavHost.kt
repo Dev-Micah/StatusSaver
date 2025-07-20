78cc5ec46989ea84863c78a0fe31a35669f5e5aa
Micah Nyabuto <atembamicah@gmail.com>
2025-07-20 15:33:48 +0300

    Update

--- app/src/main/java/com/micahnyabuto/statussaver/ui/navigation/AppNavHost.kt
+++ app/src/main/java/com/micahnyabuto/statussaver/ui/navigation/AppNavHost.kt
@@ -5,12 +5,7 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.micahnyabuto.statussaver.ui.screens.home.HomeScreen
-import com.micahnyabuto.statussaver.ui.screens.saved.SavedScreen
-import com.micahnyabuto.statussaver.ui.screens.settings.SettingsScreen
 import com.micahnyabuto.statussaver.ui.screens.splash.SplashScreen
-import com.micahnyabuto.statussaver.ui.screens.videos.VideoScreen
 
 @Composable
 fun AppNavHost(
@@ -24,35 +19,11 @@ fun AppNavHost(
         startDestination = Destinations.Splash
     ) {
         composable <Destinations.Splash>{
-            SplashScreen(
-                onSplashFinished = {
-                    navController.navigate(Destinations.Home){
-                        popUpTo(Destinations.Splash){
-                            inclusive =true
-                        }
-                    }
-                }
-            )
+            SplashScreen(navController = navController)
         }
-        composable <Destinations.Home>{
-            HomeScreen(
-                navController =navController
-            )
+        composable <Destinations.Main>{
+            MainNavGraph()
         }
-        composable <Destinations.Saved>{
-            SavedScreen(
-                navController =navController
-            )
-        }
-        composable <Destinations.Settings>{
-            SettingsScreen(
-                navController =navController
-            )
-        }
-        composable <Destinations.Videos>{
-            VideoScreen(
-                navController =navController
-            )
-        }
+
     }
 }
