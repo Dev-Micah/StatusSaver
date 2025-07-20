e0484e9e8ae57d1ef2b8304a446782c35898d6dc
Micah Nyabuto <atembamicah@gmail.com>
2025-07-20 15:33:12 +0300

    Update

--- app/src/main/java/com/micahnyabuto/statussaver/MainActivity.kt
+++ app/src/main/java/com/micahnyabuto/statussaver/MainActivity.kt
@@ -38,116 +38,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
 
         setContent {
-            val viewModel: StatusViewModel = hiltViewModel()
-            val settingsViewModel: SettingsViewModel = hiltViewModel()
-            val isDarkMode by settingsViewModel.isDarkMode.collectAsState()
-            val needsFolderPermission by viewModel.needsFolderPermission.collectAsState()
-            val scope = rememberCoroutineScope()
-
-            val context = LocalContext.current
-            val activity = context as Activity
-
-            StatusSaverTheme(darkTheme = isDarkMode) {
-                var permissionsGranted by remember { mutableStateOf(false) }
-
-                RequestStoragePermissions { permissionsGranted = true }
-
-                if (permissionsGranted) {
-                    val navController = rememberNavController()
-                    val navBackStackEntry by navController.currentBackStackEntryAsState()
-                    val currentRoute = navBackStackEntry?.destination?.route ?: Destinations.Home::class.qualifiedName.orEmpty()
-
-                    val showBottomNavigation = currentRoute !in listOf(
-                        Destinations.Splash::class.qualifiedName,
-                    )
-
-                    val launcher = rememberLauncherForActivityResult(
-                        contract = ActivityResultContracts.OpenDocumentTree()
-                    ) { uri: Uri? ->
-                        uri?.let {
-                            scope.launch {
-                                viewModel.saveSafUri(it)
-                            }
-                        }
-                    }
-
-                    LaunchedEffect(needsFolderPermission) {
-                        if (needsFolderPermission) {
-                            launcher.launch(null)
-                        }
-                    }
-
-                    Scaffold(
-                        modifier = Modifier.fillMaxSize(),
-                        contentWindowInsets = WindowInsets.navigationBars.only(WindowInsetsSides.Horizontal),
-                        bottomBar = {
-                            if (showBottomNavigation) {
-                                Column {
-                                    HorizontalDivider(thickness = 1.dp)
-                                    NavigationBar(
-                                        tonalElevation = 0.dp,
-                                        containerColor = MaterialTheme.colorScheme.surfaceVariant
-                                    ) {
-                                        BottomNavigation.entries.forEachIndexed { index, navigationItem ->
-                                            val isSelected by remember(currentRoute) {
-                                                derivedStateOf { currentRoute == navigationItem.route::class.qualifiedName }
-                                            }
-
-                                            NavigationBarItem(
-                                                selected = isSelected,
-                                                icon = {
-                                                    Column(
-                                                        horizontalAlignment = Alignment.CenterHorizontally,
-                                                        verticalArrangement = Arrangement.Center,
-                                                        modifier = Modifier.padding(vertical = 8.dp)
-                                                    ) {
-                                                        Icon(
-                                                            modifier = Modifier.size(24.dp)
-
-                                                            ,
-                                                            imageVector = (
-                                                                    if (isSelected) navigationItem.selectedIcon
-                                                                    else navigationItem.unselectedIcon
-                                                                    ),
-                                                            contentDescription = navigationItem.label
-                                                        )
-                                                        Spacer(modifier = Modifier.height(4.dp))
-                                                        Text(
-                                                            text = navigationItem.label,
-                                                            style = MaterialTheme.typography.labelSmall.copy(
-                                                                fontSize = 10.sp,
-                                                                fontWeight = if (isSelected) FontWeight.SemiBold
-                                                                else FontWeight.Normal
-                                                            )
-                                                        )
-                                                    }
-                                                },
-                                                onClick = {
-                                                    navController.navigate(navigationItem.route)
-                                                },
-                                                colors = NavigationBarItemDefaults.colors(
-                                                    indicatorColor = MaterialTheme.colorScheme.surfaceColorAtElevation(
-                                                        elevation = 0.dp
-                                                    ),
-                                                    selectedIconColor = MaterialTheme.colorScheme.primary,
-                                                    selectedTextColor = MaterialTheme.colorScheme.primary,
-                                                    unselectedIconColor = MaterialTheme.colorScheme.onSurface,
-                                                    unselectedTextColor = MaterialTheme.colorScheme.onSurface
-                                                )
-                                            )
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    ) { innerPadding ->
-                        AppNavHost(
-                            navController = navController,
-                            modifier = Modifier.padding(innerPadding)
+            val navController= rememberNavController()
+            AppNavHost(
+                            navController = navController
                         )
-                    }
+
                 }
             }
-        }
-    }
+
 }
