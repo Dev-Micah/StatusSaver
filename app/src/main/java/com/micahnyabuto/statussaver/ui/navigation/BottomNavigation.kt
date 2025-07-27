a366cf93cde1f3ebb7d833693c684371066badd9
Micah Nyabuto <atembamicah@gmail.com>
2025-07-28 00:08:02 +0300

    Fixed the Bottom bar color issue

--- app/src/main/java/com/micahnyabuto/statussaver/ui/navigation/BottomNavigation.kt
+++ app/src/main/java/com/micahnyabuto/statussaver/ui/navigation/BottomNavigation.kt
@@ -1,10 +1,8 @@
 package com.micahnyabuto.statussaver.ui.navigation
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Circle
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Image
-import androidx.compose.material.icons.filled.Save
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.ui.graphics.vector.ImageVector
 
@@ -12,24 +10,24 @@ enum class BottomNavigation(
     val label: String,
     val selectedIcon: ImageVector,
     val unselectedIcon: ImageVector,
-    val route: Any
-) {
+    val route: String
+){
     Home(
         label = "Home",
         selectedIcon = Icons.Default.Home,
         unselectedIcon = Icons.Default.Home,
-        route = Destinations.Home
+        route = Destinations.Home.route
     ),
     Saved(
         label = "Saved",
         selectedIcon = Icons.Default.Image,
         unselectedIcon = Icons.Default.Image,
-        route = Destinations.Saved
+        route = Destinations.Saved.route
     ),
     Settings(
         label = "Settings",
         selectedIcon = Icons.Default.Settings,
         unselectedIcon = Icons.Default.Settings,
-        route = Destinations.Settings
-    ),
+        route = Destinations.Settings.route
+    )
 }
