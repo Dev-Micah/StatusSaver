64820b95028b7c4decc75712dd759c96ec8853b3
Micah Nyabuto <atembamicah@gmail.com>
2025-07-28 00:08:30 +0300

    Update

--- app/src/main/java/com/micahnyabuto/statussaver/ui/navigation/AppNavHost.kt
+++ app/src/main/java/com/micahnyabuto/statussaver/ui/navigation/AppNavHost.kt
@@ -2,26 +2,25 @@ package com.micahnyabuto.statussaver.ui.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.micahnyabuto.statussaver.ui.screens.splash.SplashScreen
 
 @Composable
 fun AppNavHost(
-    navController: NavHostController,
     modifier: Modifier= Modifier
 ){
-    //val navController = rememberNavController()
+    val navController = rememberNavController()
     NavHost(
         modifier = modifier,
         navController = navController,
-        startDestination = Destinations.Splash
+        startDestination = Destinations.Splash.route
     ) {
-        composable <Destinations.Splash>{
+        composable (Destinations.Splash.route){
             SplashScreen(navController = navController)
         }
-        composable <Destinations.Main>{
+        composable (Destinations.Main.route){
             MainNavGraph()
         }
 
